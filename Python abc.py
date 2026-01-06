f0968cf1e939a276c9c6ea7817d0bfc42c0159ac
mandar.narvekar <mandar.narvekar@cytiva.com>
2026-01-06 15:02:54 +0530

    Comment file creation

---
+++ Python abc.py
@@ -0,0 +1,11 @@
+print("Hello world!")
+a=10
+b=20
+if a<b:
+    print("a is less than b")   
+elif a==b:
+    print("a is equal to b")        
+else:
+    print("a is greater than b")        
+
+    
